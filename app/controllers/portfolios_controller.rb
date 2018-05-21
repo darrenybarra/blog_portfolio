1de8e39ca6a9f99031de826a36358e32219bc32f
Darren Ybarra <darrenybarra@gmail.com>
2018-05-20 20:57:22 -0500

    refactored before action to include show for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:edit, :update, :show, :destroy]
   layout "portfolio"
   
   def index 
@@ -27,12 +28,12 @@ class PortfoliosController < ApplicationController
   end
 
   def edit 
-    @portfolio_item = Portfolio.find(params[:id])
+    set_portfolio
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-    
+    set_portfolio
+
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
@@ -43,11 +44,11 @@ class PortfoliosController < ApplicationController
   end
 
   def show 
-    @portfolio_item = Portfolio.find(params[:id])
+    set_portfolio
   end
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
+    set_portfolio    
 
     @portfolio_item.destroy
     respond_to do |format|
@@ -56,6 +57,10 @@ class PortfoliosController < ApplicationController
   end
 
   private
+
+  def set_portfolio
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
   def portfolio_params
     params.require(:portfolio).permit(:title,
